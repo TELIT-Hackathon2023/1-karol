ac22d0be43103ad5bcbf4148438ad59b3e3acffb
podmepodme <petergrocky23@gmail.com>
2023-11-25 15:52:37 +0100

    feat: added form, button and styles

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles  # Import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
